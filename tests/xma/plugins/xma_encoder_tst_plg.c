12fb5cb5867758ffae28f6a8d40447df0e735288
jnichol <jnichol@xsj-sdx-video-m4-50.xilinx.com>
2018-10-22 15:32:46 -0700

    Initializing Plugin/Kernel allocations 1. initializations 2. checks in the tests (pending from libxmaapi : test case invocation)

--- tests/xma/plugins/xma_encoder_tst_plg.c
+++ tests/xma/plugins/xma_encoder_tst_plg.c
@@ -5,6 +5,10 @@
 
 static int32_t xma_encoder_init(XmaEncoderSession *sess)
 {
+    if(*(uint8_t*)sess->base.plugin_data != 0)
+    {
+        return XMA_ERROR;
+    }
     return 0;
 }
 
@@ -27,7 +31,7 @@ static int32_t xma_encoder_close(XmaEncoderSession *sess)
 XmaEncoderPlugin encoder_plugin = {
     .hwencoder_type = XMA_COPY_ENCODER_TYPE,
     .hwvendor_string = "Xilinx",
-    .plugin_data_size = 0,
+    .plugin_data_size = 1,
     .init = xma_encoder_init,
     .send_frame = xma_encoder_send,
     .recv_data = xma_encoder_recv,
